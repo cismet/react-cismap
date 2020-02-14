377ec5702303474e0bd19404077c7d7acdb1aa3f
helllth <th@cismet.de>
2020-02-14 13:30:25 +0100

    wip on #4: improved some stories

--- src/lib/components/StyledWMSTileLayer.stories.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('StyledWMSTileLayer', module)
-	// .addDecorator(withInfo) // At your stories directly.
-	.add('First', () => <h2>First Sccess</h2>);
