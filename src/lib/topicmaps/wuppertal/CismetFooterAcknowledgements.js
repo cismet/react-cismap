6c0b36c7fb59d9ce032a7af2efa873a7d9c47a87
helllth <th@cismet.de>
2021-04-14 09:55:42 +0200

    prepare lib version

--- src/lib/topicmaps/wuppertal/CismetFooterAcknowledgements.js
+++ src/lib/topicmaps/wuppertal/CismetFooterAcknowledgements.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from "react";
 // import { getTopicMapVersion } from '../../constants/versions';
-
-const getTopicMapVersion = () => '0.0';
+import { getLibraryVersion } from "../../version";
 // Since this component is simple and static, there's no parent container for it.
 const Component = () => {
-	return (
-		<div>
-			<b>TopicMaps Wuppertal</b> (Version {getTopicMapVersion()}
-			):{' '}
-			<a href='https://cismet.de/' target='_cismet'>
-				cismet GmbH
-			</a>{' '}
-			auf Basis von{' '}
-			<a href='http://leafletjs.com/' target='_more'>
-				Leaflet
-			</a>{' '}
-			und{' '}
-			<a href='https://cismet.de/#refs' target='_cismet'>
-				cids | WuNDa
-			</a>{' '}
-			|{' '}
-			<a
-				target='_blank'
-				rel='noopener noreferrer'
-				href='https://cismet.de/datenschutzerklaerung.html'
-			>
-				Datenschutzerklärung (Privacy Policy)
-			</a>
-		</div>
-	);
+  return (
+    <div>
+      <b>TopicMaps Wuppertal</b>:{" "}
+      <a href="https://cismet.de/" target="_cismet">
+        cismet GmbH
+      </a>{" "}
+      auf Basis von{" "}
+      <a href="http://leafletjs.com/" target="_more">
+        Leaflet
+      </a>{" "}
+      und{" "}
+      <a href="https://cismet.de/#refs" target="_cismet">
+        cids | react-cismap v{getLibraryVersion()} | WuNDa
+      </a>{" "}
+      |{" "}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://cismet.de/datenschutzerklaerung.html"
+      >
+        Datenschutzerklärung (Privacy Policy)
+      </a>
+    </div>
+  );
 };
 
 export default Component;
