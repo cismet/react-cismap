1c4fb1cdae90dc971b5858effe421cda54abfe48
helllth <th@cismet.de>
2021-07-15 10:52:58 +0200

    added a infobox story with hardcoded featurecollection

--- src/lib/topicmaps/_stories/InfoBox.stories.js
+++ src/lib/topicmaps/_stories/InfoBox.stories.js
@@ -1,14 +1,27 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { storiesCategory } from './StoriesConf';
-import { RoutedMap, MappingConstants } from '../..';
-
+import React, { useState, useRef, useEffect } from "react";
+import { parkscheinautomatenfeatures, storiesCategory } from "./StoriesConf";
+import { RoutedMap, MappingConstants } from "../..";
+import TopicMapContextProvider from "../../contexts/TopicMapContextProvider";
+import TopicMapComponent from "../TopicMapComponent";
+import FeatureCollection from "../../FeatureCollection";
+import GenericInfoBoxFromFeature from "../GenericInfoBoxFromFeature";
 export default {
-	title: storiesCategory + 'InfoBox'
+  title: storiesCategory + "InfoBox",
 };
 
 const mapStyle = {
-	height: 600,
-	cursor: 'pointer'
+  height: 600,
+  cursor: "pointer",
 };
 
 export const SimpleInfoBox = () => <h3>Coming Soon</h3>;
+
+export const SimpleTopicMapWithDefaultInfoBox = () => {
+  return (
+    <TopicMapContextProvider items={parkscheinautomatenfeatures}>
+      <TopicMapComponent infoBox={<GenericInfoBoxFromFeature pixelwidth={300} />}>
+        <FeatureCollection />
+      </TopicMapComponent>
+    </TopicMapContextProvider>
+  );
+};
