2f6d1ede99975a3f429a500be22fea8309200db3
helllth <th@cismet.de>
2022-02-07 16:45:01 +0100

    serve cache if md5 lokkup does not work (eg offline)

--- src/lib/tools/fetching.js
+++ src/lib/tools/fetching.js
@@ -10,30 +10,37 @@ const noCacheInit = {
 
 export const md5FetchJSON = async (prefix, uri) => {
   console.log("uri to fetch", uri);
-
-  let md5 = await (await fetch(uri + ".md5", noCacheInit)).text();
   try {
-    const md5InCache = await localforage.getItem("@" + prefix + ".." + uri + ".md5");
-    if (md5InCache !== null && md5InCache === md5) {
-      console.log("cache hit: " + uri);
-      const jsonStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
-      return new Promise((resolve, reject) => {
-        resolve(JSON.parse(jsonStringInCache));
-      });
-    } else {
-      console.log("cache miss" + uri);
+    let md5 = await (await fetch(uri + ".md5", noCacheInit)).text();
+    try {
+      const md5InCache = await localforage.getItem("@" + prefix + ".." + uri + ".md5");
+      if (md5InCache !== null && md5InCache === md5) {
+        console.log("cache hit: " + uri);
+        const jsonStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
+        return new Promise((resolve, reject) => {
+          resolve(JSON.parse(jsonStringInCache));
+        });
+      } else {
+        console.log("cache miss" + uri);
+        const data = await (await fetch(uri)).json();
+        await localforage.setItem("@" + prefix + ".." + uri, JSON.stringify(data));
+        await localforage.setItem("@" + prefix + ".." + uri + ".md5", md5);
+        return new Promise((resolve, reject) => {
+          resolve(data);
+        });
+      }
+    } catch (e) {
+      console.log("cache lookup error", e);
       const data = await (await fetch(uri)).json();
-      await localforage.setItem("@" + prefix + ".." + uri, JSON.stringify(data));
-      await localforage.setItem("@" + prefix + ".." + uri + ".md5", md5);
       return new Promise((resolve, reject) => {
         resolve(data);
       });
     }
   } catch (e) {
-    console.log("cache lookup error", e);
-    const data = await (await fetch(uri)).json();
+    console.log("md5 lookup error. try to server cache directly");
+    const jsonStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
     return new Promise((resolve, reject) => {
-      resolve(data);
+      resolve(JSON.parse(jsonStringInCache));
     });
   }
 };
@@ -81,30 +88,37 @@ export const fetchJSON = async (uri) => {
 
 export const md5FetchText = async (prefix, uri) => {
   console.log("uri to fetch", uri);
-
-  let md5 = await (await fetch(uri + ".md5", noCacheInit)).text();
   try {
-    const md5InCache = await localforage.getItem("@" + prefix + ".." + uri + ".md5");
-    if (md5InCache !== null && md5InCache === md5) {
-      console.log("cache hit");
-      const textStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
-      return new Promise((resolve, reject) => {
-        resolve(textStringInCache);
-      });
-    } else {
-      console.log("cache miss");
+    let md5 = await (await fetch(uri + ".md5", noCacheInit)).text();
+    try {
+      const md5InCache = await localforage.getItem("@" + prefix + ".." + uri + ".md5");
+      if (md5InCache !== null && md5InCache === md5) {
+        console.log("cache hit");
+        const textStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
+        return new Promise((resolve, reject) => {
+          resolve(textStringInCache);
+        });
+      } else {
+        console.log("cache miss");
+        const data = await (await fetch(uri)).text();
+        await localforage.setItem("@" + prefix + ".." + uri, data);
+        await localforage.setItem("@" + prefix + ".." + uri + ".md5", md5);
+        return new Promise((resolve, reject) => {
+          resolve(data);
+        });
+      }
+    } catch (e) {
+      console.log("cache lookup error", e);
       const data = await (await fetch(uri)).text();
-      await localforage.setItem("@" + prefix + ".." + uri, data);
-      await localforage.setItem("@" + prefix + ".." + uri + ".md5", md5);
       return new Promise((resolve, reject) => {
         resolve(data);
       });
     }
   } catch (e) {
-    console.log("cache lookup error", e);
-    const data = await (await fetch(uri)).text();
+    console.log("md5 lookup error. try to server cache directly");
+    const textStringInCache = await localforage.getItem("@" + prefix + ".." + uri);
     return new Promise((resolve, reject) => {
-      resolve(data);
+      resolve(textStringInCache);
     });
   }
 };
