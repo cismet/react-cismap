30116f53014f72dc3cde076c71eb29de81383817
helllth <th@cismet.de>
2022-03-18 16:55:13 +0100

    remove layerKeyPostfix from key in layer div to avoid unnecessary rerendering

--- src/lib/RoutedMap.js
+++ src/lib/RoutedMap.js
@@ -481,7 +481,9 @@ export class RoutedMap extends React.Component {
               "." +
               md5(this.props.baseLayerConf || "") +
               "." +
-              this.props.layerKeyPostfix
+              // this.props.layerKeyPostfix +
+              "." +
+              this.props.offlineReadyToUse
             }
           >
             {getLayersByNames(
