59756ee683d43806ee49a7114afc81333bc327a0
helllth <th@cismet.de>
2022-03-21 12:38:19 +0100

    only set value if it is not undefined or null

--- src/lib/contexts/_helper.js
+++ src/lib/contexts/_helper.js
@@ -1,10 +1,10 @@
 import localforage from "localforage";
 
-export const setFromLocalforage = async (lfKey, setter, fallbackValue) => {
+export const setFromLocalforage = async (lfKey, setter, fallbackValue, forceFallback) => {
   const value = await localforage.getItem(lfKey);
-  if (value !== undefined) {
+  if (value !== undefined && value !== null) {
     setter(value);
-  } else if (fallbackValue !== undefined) {
+  } else if (fallbackValue !== undefined || forceFallback === true) {
     setter(fallbackValue);
   }
 };
