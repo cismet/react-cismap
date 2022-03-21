d1dcd6e54dd5d1725f04e7f80b13606292839e4d
helllth <th@cismet.de>
2022-03-21 12:31:33 +0100

    only ste fallbackvalue if it is !== undefined

--- src/lib/contexts/_helper.js
+++ src/lib/contexts/_helper.js
@@ -4,7 +4,7 @@ export const setFromLocalforage = async (lfKey, setter, fallbackValue) => {
   const value = await localforage.getItem(lfKey);
   if (value !== undefined) {
     setter(value);
-  } else {
+  } else if (fallbackValue !== undefined) {
     setter(fallbackValue);
   }
 };
