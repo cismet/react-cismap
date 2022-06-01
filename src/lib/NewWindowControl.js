9480fe2830bf30ea4c2cd45f0b0ccfadedae911b
helllth <th@cismet.de>
2022-06-01 10:56:14 +0200

    using a local version of easy-button to prevent that the NewWIndowComtrol poens two new indows on ios

--- src/lib/NewWindowControl.js
+++ src/lib/NewWindowControl.js
@@ -1,31 +1,31 @@
-import PropTypes from 'prop-types';
-import 'leaflet-easybutton';
-import 'leaflet-easybutton/src/easy-button.css';
-import './EasyButtonOverrides.css';
+import PropTypes from "prop-types";
+import "./custom/easy-button";
+import "leaflet-easybutton/src/easy-button.css";
+import "./EasyButtonOverrides.css";
 
-import { MapControl } from 'react-leaflet';
-import L from 'leaflet';
+import { MapControl } from "react-leaflet";
+import L from "leaflet";
 
 class NewWindowControl extends MapControl {
-	componentWillMount() {
-		let that = this;
-		this.leafletElement = L.easyButton(
-			'fa-external-link-square-alt',
-			function(btn, map) {
-				window.open(window.location.href);
-			},
-			this.props.title,
-			{
-				position: this.props.position || 'topleft'
-			}
-		);
-	}
+  componentWillMount() {
+    let that = this;
+    this.leafletElement = L.easyButton(
+      "fa-external-link-square-alt",
+      function (btn, map) {
+        window.open(window.location.href);
+      },
+      this.props.title,
+      {
+        position: this.props.position || "topleft",
+      }
+    );
+  }
 }
 
 NewWindowControl.propTypes = {
-	position: PropTypes.string,
-	title: PropTypes.string,
-	routing: PropTypes.object
+  position: PropTypes.string,
+  title: PropTypes.string,
+  routing: PropTypes.object,
 };
 
 export default NewWindowControl;
