92672d62a0e33ce6f1e80ca739b5c0450505c80e
helllth <th@cismet.de>
2022-07-20 17:34:31 +0200

    added footer and license text for new true orthofoto

--- src/lib/topicmaps/wuppertal/ModalMenuFooter.js
+++ src/lib/topicmaps/wuppertal/ModalMenuFooter.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import CismetFooterAcks from './CismetFooterAcknowledgements';
+import React from "react";
+import CismetFooterAcks from "./CismetFooterAcknowledgements";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 const Comp = ({ showModalMenu }) => {
-	return (
-		<div style={{ fontSize: '11px' }}>
-			<b>Hintergrundkarten</b>: Stadtkarte 2.0 © RVR | True Orthophoto 2020 © Stadt Wuppertal {' '}
-			<a onClick={() => showModalMenu('help')}>(Details und Nutzungsbedingungen)</a>
-			<br />
-			<CismetFooterAcks />
-		</div>
-	);
+  return (
+    <div style={{ fontSize: "11px" }}>
+      <b>Hintergrundkarten</b>: Stadtkarte 2.0 © RVR | True Orthophoto 2022 © Stadt Wuppertal{" "}
+      <a onClick={() => showModalMenu("help")}>(Details und Nutzungsbedingungen)</a>
+      <br />
+      <CismetFooterAcks />
+    </div>
+  );
 };
 
 export default Comp;
 Comp.defaultProps = {
-	showModalMenu: () => {}
+  showModalMenu: () => {},
 };
