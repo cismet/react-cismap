c1bdf36472d4a83aeac5faa0a666a53747298ec3
Thorsten Hell <thorsten.hell@cismet.de>
2023-11-28 11:53:12 +0100

    added opacityFunction

--- src/lib/CismapLayer.js
+++ src/lib/CismapLayer.js
@@ -15,26 +15,38 @@ const defaults = {
   },
   vector: {},
 };
-export default function CismapLayer(props) {
-  switch (props.type) {
-    case "wms":
-    case "wmts": {
-      let params = { ...defaults.wms, ...props };
-      return <StyledWMSTileLayer {...params} />;
-    }
-    case "wms-nt":
-    case "wmts-nt": {
-      let params = { ...defaults.wms, ...props };
-      return <NonTiledWMSLayer {...params} />;
-    }
 
-    case "tiles": {
-      let params = { ...defaults.wms, ...props };
-      return <TileLayer {...params} />;
+export default function CismapLayer(_props) {
+  const props = JSON.parse(JSON.stringify(_props));
+  //function gets not copied
+  if (props.type === undefined) {
+    console.error("CismapLayer: type not set", props);
+    return null;
+  } else {
+    let opacity = props.opacity || 1;
+    if (_props.opacityFunction) {
+      opacity = _props.opacityFunction(opacity);
     }
-    case "vector": {
-      let params = { ...defaults.vector, ...props };
-      return <MapLibreLayer {...params} />;
+    switch (props.type) {
+      case "wms":
+      case "wmts": {
+        let params = { ...defaults.wms, ...props, opacity };
+        return <StyledWMSTileLayer {...params} />;
+      }
+      case "wms-nt":
+      case "wmts-nt": {
+        let params = { ...defaults.wms, ...props, opacity };
+        return <NonTiledWMSLayer {...params} />;
+      }
+
+      case "tiles": {
+        let params = { ...defaults.wms, ...props, opacity };
+        return <TileLayer {...params} />;
+      }
+      case "vector": {
+        let params = { ...defaults.vector, ...props, opacity };
+        return <MapLibreLayer {...params} />;
+      }
     }
   }
 }
