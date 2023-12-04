5215402aa5fdc2c7ae05a51c2ea3c9d25f02a9e4
Thorsten Hell <thorsten.hell@cismet.de>
2023-12-04 14:16:37 +0100

    add hideWhenNoSibblingIsPresent prop (defaul false)

--- src/lib/CrossTabCommunicationControl.js
+++ src/lib/CrossTabCommunicationControl.js
@@ -15,69 +15,72 @@ const Hover = ({ onHover, children }) => (
   </div>
 );
 
-export default function ({ showConnectionCount = false }) {
+export default function ({ showConnectionCount = false, hideWhenNoSibblingIsPresent = false }) {
   const { isDynamicLeader, isPaused, connectedEntities } = useContext(CrossTabCommunicationContext);
   const { setPaused } = useContext(CrossTabCommunicationDispatchContext);
-
-  return (
-    <>
-      <Control className="leaflet-bar leaflet-control hover-control" position="topleft">
-        <>
-          <span className="hover-control--off">
-            <a
-              className="leaflet-bar-part"
-              title="Vollbildmodus"
-              style={{ outline: "none", background: isDynamicLeader ? "lightgrey" : undefined }}
-            >
-              <span>
-                <FontAwesomeIcon icon={faSync} size="lg" />
-                {!isPaused && showConnectionCount && (
-                  <span
-                    className="fa-layers-counter  fa-layers-bottom-right"
-                    style={{
-                      fontSize: 50 + "px",
-                      position: "absolute",
-                      bottom: -10,
-                      right: -10,
-                      backgroundColor: "#555555",
-                    }}
-                  >
-                    {connectedEntities.length + 1}
-                  </span>
-                )}
-              </span>
-            </a>
-          </span>
-          <span class="hover-control--on">
-            <a
-              className="leaflet-bar-part"
-              title="Vollbildmodus"
-              style={{ outline: "none", background: isDynamicLeader ? "lightgrey" : undefined }}
-              onClick={() => {
-                setPaused(!isPaused);
-              }}
-            >
-              <span>
-                <FontAwesomeIcon icon={isPaused ? faPlay : faPause} size="lg" />
-                {!isPaused && showConnectionCount && (
-                  <span
-                    className="fa-layers-counter  fa-layers-bottom-right"
-                    style={{
-                      fontSize: 50 + "px",
-                      position: "absolute",
-                      bottom: -10,
-                      right: -10,
-                      backgroundColor: "#555555",
-                    }}
-                  >
-                    {connectedEntities.length + 1}
-                  </span>
-                )}
-              </span>
-            </a>
-          </span>
-        </>
-      </Control>
-    </>
-  );
+  if (hideWhenNoSibblingIsPresent && connectedEntities.length === 0) {
+    return null;
+  } else {
+    return (
+      <>
+        <Control className="leaflet-bar leaflet-control hover-control" position="topleft">
+          <>
+            <span className="hover-control--off">
+              <a
+                className="leaflet-bar-part"
+                title="Vollbildmodus"
+                style={{ outline: "none", background: isDynamicLeader ? "lightgrey" : undefined }}
+              >
+                <span>
+                  <FontAwesomeIcon icon={faSync} size="lg" />
+                  {!isPaused && showConnectionCount && (
+                    <span
+                      className="fa-layers-counter  fa-layers-bottom-right"
+                      style={{
+                        fontSize: 50 + "px",
+                        position: "absolute",
+                        bottom: -10,
+                        right: -10,
+                        backgroundColor: "#555555",
+                      }}
+                    >
+                      {connectedEntities.length + 1}
+                    </span>
+                  )}
+                </span>
+              </a>
+            </span>
+            <span class="hover-control--on">
+              <a
+                className="leaflet-bar-part"
+                title="Vollbildmodus"
+                style={{ outline: "none", background: isDynamicLeader ? "lightgrey" : undefined }}
+                onClick={() => {
+                  setPaused(!isPaused);
+                }}
+              >
+                <span>
+                  <FontAwesomeIcon icon={isPaused ? faPlay : faPause} size="lg" />
+                  {!isPaused && showConnectionCount && (
+                    <span
+                      className="fa-layers-counter  fa-layers-bottom-right"
+                      style={{
+                        fontSize: 50 + "px",
+                        position: "absolute",
+                        bottom: -10,
+                        right: -10,
+                        backgroundColor: "#555555",
+                      }}
+                    >
+                      {connectedEntities.length + 1}
+                    </span>
+                  )}
+                </span>
+              </a>
+            </span>
+          </>
+        </Control>
+      </>
+    );
+  }
 }
