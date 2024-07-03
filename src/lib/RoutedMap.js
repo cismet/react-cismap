4a71181626dc5766d88e620535f873e4d465fbc7
Thorsten Hell <thorsten.hell@cismet.de>
2024-07-03 15:53:36 +0200

    add 100 predefined panes for additionalLayers

--- src/lib/RoutedMap.js
+++ src/lib/RoutedMap.js
@@ -150,16 +150,12 @@ export class RoutedMap extends React.Component {
     map.createPane("backgroundLayers");
     map.getPane("backgroundLayers").style.zIndex = 100;
 
-    map.createPane("additionalLayers0");
-    map.getPane("additionalLayers0").style.zIndex = 250;
-    map.createPane("additionalLayers1");
-    map.getPane("additionalLayers1").style.zIndex = 250;
-    map.createPane("additionalLayers2");
-    map.getPane("additionalLayers2").style.zIndex = 250;
-    map.createPane("additionalLayers3");
-    map.getPane("additionalLayers3").style.zIndex = 250;
-    map.createPane("additionalLayers4");
-    map.getPane("additionalLayers4").style.zIndex = 250;
+    // create 100 addi´ítionalLayers and increment the z-index by 1
+    for (let i = 0; i < 100; i++) {
+      map.createPane("additionalLayers" + i);
+      map.getPane("additionalLayers" + i).style.zIndex = 250 + i;
+    }
+
     map.createPane("backgroundlayerTooltips");
     map.getPane("backgroundlayerTooltips").style.zIndex = 550;
 
