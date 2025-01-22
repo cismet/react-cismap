834d11f117e998f63937dde938580cd0bf048d6b
Thorsten Hell <thorsten.hell@cismet.de>
2025-01-22 16:03:40 +0100

    added additionalLayerUniquePane and additionalLayersFreeZOrder to use zINdex for reordering (much faster and no flickering)

--- src/lib/CismapLayer.js
+++ src/lib/CismapLayer.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import StyledWMSTileLayer from "./StyledWMSTileLayer";
 import NonTiledWMSLayer from "./NonTiledWMSLayer";
 import { TileLayer } from "react-leaflet";
 import MapLibreLayer from "./vector/MapLibreLayer";
 import GraphqlLayer from "./GraphqlLayer";
+import { TopicMapContext } from "./contexts/TopicMapContextProvider";
+import { use } from "react";
 
 const defaults = {
   wms: {
@@ -18,11 +20,39 @@ const defaults = {
 };
 
 export default function CismapLayer(props) {
+  const { routedMapRef } = useContext(TopicMapContext);
+  const [additionalLayerUniquePaneCreated, setAdditionalLayerUniquePaneCreated] = useState(false);
+  let paneName = props.pane;
+  if (props.additionalLayerUniquePane) {
+    paneName = "dynamic-additionalLayer-" + props.additionalLayerUniquePane;
+  }
+
+  useEffect(() => {
+    if (routedMapRef && props.additionalLayerUniquePane) {
+      const leafletMap = routedMapRef.leafletMap.leafletElement;
+      if (leafletMap && !leafletMap.getPane(paneName)) {
+        // console.log('xxx try to createPane(paneName); ', paneName, props.additionalLayersFreeZOrder);
+        leafletMap.createPane(paneName);
+        //set zIndex to 250 + props.additionalLayersFreeZOrder
+        leafletMap.getPane(paneName).style.zIndex = 250 + props.additionalLayersFreeZOrder
+        // console.log('xxx tried to createPane(paneName); ', leafletMap.getPane(paneName));
+        setAdditionalLayerUniquePaneCreated(true);
+      } else if (leafletMap && leafletMap.getPane(paneName)) {
+        // console.log('xxx pane already exists', paneName, props.additionalLayersFreeZOrder);
+        leafletMap.getPane(paneName).style.zIndex = 250 + props.additionalLayersFreeZOrder;
+
+      }
+
+    }
+
+
+  }, [routedMapRef, props.additionalLayerUniquePane, props.additionalLayersFreeZOrder]);
+
+
   if (props.type === undefined) {
     console.error("CismapLayer: type not set", props);
     return null;
-  } else {
-
+  } else if (routedMapRef && (props.additionalLayerUniquePane === undefined || additionalLayerUniquePaneCreated === true)) {
     let opacity = props.opacity;
     if (opacity === undefined || opacity === null) {
       opacity = 1;
@@ -33,25 +63,25 @@ export default function CismapLayer(props) {
     switch (props.type) {
       case "wms":
       case "wmts": {
-        let params = { ...defaults.wms, ...props, opacity };
+        let params = { ...defaults.wms, ...props, opacity, pane: paneName };
         return <StyledWMSTileLayer {...params} />;
       }
       case "wms-nt":
       case "wmts-nt": {
-        let params = { ...defaults.wms, ...props, opacity };
+        let params = { ...defaults.wms, ...props, opacity, pane: paneName };
         return <NonTiledWMSLayer {...params} />;
       }
 
       case "tiles": {
-        let params = { ...defaults.wms, ...props, opacity };
+        let params = { ...defaults.wms, ...props, opacity, pane: paneName };
         return <TileLayer {...params} />;
       }
       case "vector": {
-        let params = { ...defaults.vector, ...props, opacity };
+        let params = { ...defaults.vector, ...props, opacity, pane: paneName };
         return <MapLibreLayer {...params} />;
       }
       case "graphql": {
-        let params = { ...defaults.graphql, ...props, opacity };
+        let params = { ...defaults.graphql, ...props, opacity, pane: paneName };
         return <GraphqlLayer {...params} />;
       }
     }
