7c0bdc2b682ffcfca3e09f0fa8216d43848daca0
David Glogaza <david.glogaza40@googlemail.com>
2025-01-31 12:33:46 +0100

    try to remove click listener when removing layer

--- src/lib/vector/MapLibreLayerBaseComponent.js
+++ src/lib/vector/MapLibreLayerBaseComponent.js
@@ -60,6 +60,9 @@ class MaplibreGlLayer extends GridLayer {
       if (e.layer === this.leafletElement) {
         this._removeLayer(e);
         map.off("layerremove");
+
+        //clean up the local click listener
+        map.off("click", myClickListener);
       }
     });
     // map.on("moveend", (e) => {
@@ -71,100 +74,109 @@ class MaplibreGlLayer extends GridLayer {
     const maxSelectionCount = props.maxSelectionCount || 1;
     const normalizeFeatureHitsById = props.normalizeFeatureHitsById || false;
     const manualSelectionManagement = props.manualSelectionManagement || false;
-    if (props.onSelectionChanged) {
-      map.on("click", (e) => {
-        if (this.selectionLayerExists || manualSelectionManagement) {
-          if (this.mapLibreMap?.project) {
-            // Project the clicked point to map coordinates
-            const point = this.mapLibreMap.project([e.latlng.lng, e.latlng.lat]);
+    const myClickListener = (e) => {
+      if (this.selectionLayerExists || manualSelectionManagement) {
+        if (this.mapLibreMap?.project) {
+          // Project the clicked point to map coordinates
+          const point = this.mapLibreMap.project([e.latlng.lng, e.latlng.lat]);
 
-            // Create a small bounding box around the clicked point
-            const size = 0;
-            const rect = [
-              [point.x - size, point.y - size],
-              [point.x + size, point.y + size],
-            ];
+          // Create a small bounding box around the clicked point
+          const size = 0;
+          const rect = [
+            [point.x - size, point.y - size],
+            [point.x + size, point.y + size],
+          ];
 
-            // Convert the bounding box points back to latitude and longitude
-            const queryRect = rect.map((p) => this.mapLibreMap.unproject(p));
+          // Convert the bounding box points back to latitude and longitude
+          const queryRect = rect.map((p) => this.mapLibreMap.unproject(p));
 
-            // console.log("xxx adjusted point:", point.x, point.y);
-            // console.log("xxx this.mapLibreMap", this.mapLibreMap);
+          // console.log("xxx adjusted point:", point.x, point.y);
+          // console.log("xxx this.mapLibreMap", this.mapLibreMap);
 
-            if (this.mapLibreMap && this.props.selectionEnabled === true) {
-              const hits = this.mapLibreMap.queryRenderedFeatures(rect);
-              const filteredHits = hits.filter((hit) => {
-                //hit.layer.id should not contain selection
-                return !hit.layer.id.includes("selection");
-              });
+          if (this.mapLibreMap && this.props.selectionEnabled === true) {
+            const hits = this.mapLibreMap.queryRenderedFeatures(rect);
+            const filteredHits = hits.filter((hit) => {
+              //hit.layer.id should not contain selection
+              return !hit.layer.id.includes("selection");
+            });
 
-              // Deselect all selected features first
-              selectedFeatures.forEach((feature) => {
-                try {
-                  this.mapLibreMap.setFeatureState(
-                    { source: feature.source, sourceLayer: feature.sourceLayer, id: feature.id },
-                    { selected: false }
-                  );
-                } catch (e) {}
-              });
-
-              selectedFeatures.clear();
-
-              if (filteredHits.length > 0) {
-                // Limit the selection to maxSelectionCount
-
-                const limitedHits = filteredHits.slice(0, maxSelectionCount);
-
-                const normalizedLimitedHits = [];
-                limitedHits.forEach((hit) => {
-                  // console.log("xxx -> ", hit.layer.id, hit.properties.id, hit);
-
-                  const setSelection = (selected) => {
-                    this.mapLibreMap.setFeatureState(
-                      { source: hit.source, sourceLayer: hit.sourceLayer, id: hit.id },
-                      { selected }
-                    );
-                    selectedFeatures.add({
-                      source: hit.source,
-                      sourceLayer: hit.sourceLayer,
-                      id: hit.id,
-                    });
-                  };
-                  if (manualSelectionManagement === false) {
-                    setSelection(true);
-                  } else {
-                    hit.setSelection = setSelection;
-                    hit.selectionLayerExists = this.selectionLayerExists;
-                  }
-
-                  //add hit to normalizedLimitedHits if an object with the id isn't already in the array
-                  if (!normalizedLimitedHits.some((e) => e.id === hit.id)) {
-                    normalizedLimitedHits.push(hit);
-                  }
-
-                  // console.log(`State set for feature ID ${hit.id}`);
-                });
-                // console.log('limitedHits', limitedHits);
-                // console.log('normalizedLimitedHits', normalizedLimitedHits);
-
-                if (normalizeFeatureHitsById) {
-                  props.onSelectionChanged({
-                    hits: normalizedLimitedHits,
-                    hit: normalizedLimitedHits[0],
-                  });
-                } else {
-                  props.onSelectionChanged({ hits: limitedHits, hit: limitedHits[0] });
-                }
-              } else {
-                props.onSelectionChanged({ hits: undefined, hit: undefined });
-                // console.log("No features found at the click location.");
+            // Deselect all selected features first
+            selectedFeatures.forEach((feature) => {
+              try {
+                this.mapLibreMap.setFeatureState(
+                  { source: feature.source, sourceLayer: feature.sourceLayer, id: feature.id },
+                  { selected: false }
+                );
+              } catch (e) {
+                console.error("xxx deselect error", e);
               }
+            });
+
+            selectedFeatures.clear();
+
+            if (filteredHits.length > 0) {
+              // Limit the selection to maxSelectionCount
+
+              const limitedHits = filteredHits.slice(0, maxSelectionCount);
+
+              const normalizedLimitedHits = [];
+              limitedHits.forEach((hit) => {
+                // console.log("xxx -> ", hit.layer.id, hit.properties.id, hit);
+
+                const setSelection = (selected) => {
+                  this.mapLibreMap.setFeatureState(
+                    { source: hit.source, sourceLayer: hit.sourceLayer, id: hit.id },
+                    { selected }
+                  );
+                  selectedFeatures.add({
+                    source: hit.source,
+                    sourceLayer: hit.sourceLayer,
+                    id: hit.id,
+                  });
+                };
+                console.log("xxx ", {
+                  source: hit.source,
+                  sourceLayer: hit.sourceLayer,
+                  id: hit.id,
+                });
+
+                if (manualSelectionManagement === false) {
+                  setSelection(true);
+                } else {
+                  hit.setSelection = setSelection;
+                  hit.selectionLayerExists = this.selectionLayerExists;
+                }
+
+                //add hit to normalizedLimitedHits if an object with the id isn't already in the array
+                if (!normalizedLimitedHits.some((e) => e.id === hit.id)) {
+                  normalizedLimitedHits.push(hit);
+                }
+
+                // console.log(`State set for feature ID ${hit.id}`);
+              });
+              // console.log('limitedHits', limitedHits);
+              // console.log('normalizedLimitedHits', normalizedLimitedHits);
+
+              if (normalizeFeatureHitsById) {
+                props.onSelectionChanged({
+                  hits: normalizedLimitedHits,
+                  hit: normalizedLimitedHits[0],
+                });
+              } else {
+                props.onSelectionChanged({ hits: limitedHits, hit: limitedHits[0] });
+              }
+            } else {
+              props.onSelectionChanged({ hits: undefined, hit: undefined });
+              // console.log("No features found at the click location.");
             }
-          } else {
-            console.log("xxx no mapLibreMap set");
           }
+        } else {
+          console.log("xxx no mapLibreMap set");
         }
-      });
+      }
+    };
+    if (props.onSelectionChanged) {
+      map.on("click", myClickListener);
     }
 
     // if (props.offlineAvailable) {
