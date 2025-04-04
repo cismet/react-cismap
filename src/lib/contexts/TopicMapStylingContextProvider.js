6af983517cbd66dd94701e1321c46a64c62c5105
Thorsten Hell <thorsten.hell@cismet.de>
2025-04-04 09:43:13 +0200

    switched lbk definition from trueOrtho2022 to trueOrtho2024

--- src/lib/contexts/TopicMapStylingContextProvider.js
+++ src/lib/contexts/TopicMapStylingContextProvider.js
@@ -40,8 +40,8 @@ const defaultBackgroundConfigurations = {
     title: "Top. Karte",
   },
   lbk: {
-    // layerkey: "rvrGrundriss@100|trueOrtho2020@75|rvrSchrift@100",
-    layerkey: "rvrGrundriss@100|trueOrtho2022@75|rvrSchriftNT@100",
+    // layerkey: "rvrGrundriss@100|trueOrtho2022@75|rvrSchriftNT@100",
+    layerkey: "rvrGrundriss@100|trueOrtho2024@75|rvrSchriftNT@100",
     src: "/images/rain-hazard-map-bg/ortho.png",
     title: "Luftbildkarte",
   },
