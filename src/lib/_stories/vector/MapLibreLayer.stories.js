5143671c7f0faa81bdeb117bd4aabbde8f4515dd
Thorsten Hell <thorsten.hell@cismet.de>
2025-11-25 15:34:52 +0100

    set logMapLibreDebugLogs to true to show intense hiding logging

--- src/lib/_stories/vector/MapLibreLayer.stories.js
+++ src/lib/_stories/vector/MapLibreLayer.stories.js
@@ -260,12 +260,14 @@ export const SimpleTopicMapWithMapLibreLayerToTestHidingForwarding = () => {
         <CismapLayer
           {...{
             type: "vector",
+            //style: "https://tiles.cismet.de/alkis/flurstuecke.yellow.style.json",
             // style: "https://tiles.cismet.de/alkis/flurstuecke.str.hsnr.yellow.style.json",
             style: "https://tiles.cismet.de/alkis/xxx.flurstuecke.yellow.style.json",
             additionalLayerUniquePane: "bplan",
             opacity: 1,
             additionalLayersFreeZOrder: 9,
             logMapLibreErrors: true,
+            logMapLibreDebugLogs: true,
           }} />
       </TopicMapComponent>
     </TopicMapContextProvider>
